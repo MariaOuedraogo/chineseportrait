19b3667878049c2c17311860d1c670dfe5a2452f
Maria Ouedraogo <maria.ouedraogo.pro@gmail.com>
2024-09-21 15:33:05 +0200

    fixed : script.js
    Adjust the .header color transition between the .hero and .analogies sections.

--- scripts/index.js
+++ scripts/index.js
@@ -6,7 +6,7 @@ gsap.registerPlugin(ScrollTrigger);
 
 ScrollTrigger.create({
     trigger: ".analogies",
-    start: "top top", // When the top of the analogies section reaches the top of the window
+    start: "top-=50px top", // When the top of the analogies section reaches the top of the window
     end: "bottom top", // When the bottom of the analogies section reaches the top of the window
     onEnter: () => gsap.to(".header", { color: '#f52571', duration: 0.5 }),
     onLeaveBack: () => gsap.to(".header", { color: '', duration: 0.5 }),
